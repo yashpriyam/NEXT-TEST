ca3834898d7c57bc5ff3b9fb879e0f3bb127309b
yashpriyam <yashpriyam74@gmail.com>
2024-09-23 18:18:59 +0530

    add image

--- src/app/components/Test.tsx
+++ src/app/components/Test.tsx
@@ -1,11 +1,24 @@
 import ParentTest from "./ParentTest";
+import Image from 'next/image';
 
 const Test = () => {
-    console.log("test component")
-    return <> <div>Test</div>
-    <ParentTest />
-    </> 
-    
+    console.log("test component");
+
+    return (
+        <>
+            <div>Test</div>
+            
+            {/* Adding a placeholder image from Unsplash */}
+            <Image 
+                src="https://images.unsplash.com/photo-1593642634367-d91a135587b5"
+                alt="Placeholder image"
+                width={500}
+                height={300}
+            />
+            
+            <ParentTest />
+        </>
+    );
 }
 
 export default Test;
