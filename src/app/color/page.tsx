2533852cf6cd51c3b5c4b660e1b25da0d6ea9423
yashpriyam <yashpriyam74@gmail.com>
2024-09-25 11:07:53 +0530

    add client compponent inside color page

--- src/app/color/page.tsx
+++ src/app/color/page.tsx
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic';
 import React, {  useState } from 'react';
 const Test=dynamic(()=>import("../components/Test"),{ssr:true})
  import { ChromePicker } from 'react-color';
+const ClientWrapper=dynamic(()=>import("../ClientWrapper/page"),{ssr:false})
 // const ChromePicker=dynamic(()=>import("react-color").then(mod=>mod.ChromePicker))
 const ColorPickerPage = ({children} :any) => {
   const [color, setColor] = useState('#ff0000');
@@ -18,6 +19,7 @@ const ColorPickerPage = ({children} :any) => {
         onChangeComplete={(newColor) => setColor(newColor.hex)}
       />
       <Test />
+      <ClientWrapper />
       {/* {children} */}
      
       <p>Selected Color: {color}</p>
